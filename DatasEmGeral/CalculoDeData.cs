354e977c0c0ad7b580bb2580b79dc4187be51481
leandrorochaadm <leandrorochaadm@gmail.com>
2020-12-01 20:55:12 -0400

    data string

--- DatasEmGeral/CalculoDeData.cs
+++ DatasEmGeral/CalculoDeData.cs
@@ -14,6 +14,27 @@ namespace LeandroAT1.DatasEmGeral
             TimeSpan difereca = dataFinal - dataInicial;
             return difereca.Days;
         }
+
+        public static int DifEntreDatasEmDias(string dataIn, string dataFin)
+        {
+            int diaI, mesI, anoI, diaF, mesF, anoF;
+
+            //data inicial
+            diaI = Convert.ToInt32(dataIn.Substring(0, 2));
+            mesI = Convert.ToInt32(dataIn.Substring(3, 2));
+            anoI = Convert.ToInt32(dataIn.Substring(6, 4));
+            
+            //data final
+            diaF = Convert.ToInt32(dataFin.Substring(0, 2));
+            mesF = Convert.ToInt32(dataFin.Substring(3, 2));
+            anoF = Convert.ToInt32(dataFin.Substring(6, 4));
+
+            DateTime dataInicial = new DateTime(anoI, mesI, diaI);
+            DateTime dataFinal = new DateTime(anoF, mesF, diaF);
+
+            TimeSpan difereca = dataFinal - dataInicial;
+            return difereca.Days;
+        }
        /* public static int DifDataInicialEDataDeHojeEmMeses() { return 0; }
         public static int DifDataInicialEDataDeHojeEmAnos() { return 0; }
         public void SomarDiasParaUmaData();
--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace LeandroAT1
         static void Main(string[] args)
         {
             Console.WriteLine("dias!");
-            Console.WriteLine(CalculoDeData.DifEntreDatasEmDias(1,1,2020,1,2,2020));        }
+            Console.WriteLine(CalculoDeData.DifEntreDatasEmDias(1,1,2020,1,2,2020));
+            Console.WriteLine(CalculoDeData.DifEntreDatasEmDias("01/01/2020","01/02/2020"));
+        }
     }
 }
