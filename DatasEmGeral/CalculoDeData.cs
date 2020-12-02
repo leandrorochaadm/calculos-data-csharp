2d8137d958f0b0112e316f9cbb61d365b6b717b3
leandrorochaadm <leandrorochaadm@gmail.com>
2020-12-01 20:31:19 -0400

    criacao do projeto

---
+++ DatasEmGeral/CalculoDeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeandroAT1.DatasEmGeral
+{
+    public static class CalculoDeData
+    {
+        public static int DifEntreDatasEmDias(int diaI, int mesI, int anoI, int diaF, int mesF, int anoF)
+        {
+            DateTime dataInicial = new DateTime(anoI, mesI, diaI);
+            DateTime dataFinal = new DateTime(anoF, mesF, diaF);
+
+            TimeSpan difereca = dataFinal - dataInicial;
+            return difereca.Days;
+        }
+       /* public static int DifDataInicialEDataDeHojeEmMeses() { return 0; }
+        public static int DifDataInicialEDataDeHojeEmAnos() { return 0; }
+        public void SomarDiasParaUmaData();
+        public void SomarDiasApartirDataAtual();
+       */
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using LeandroAT1.DatasEmGeral;
+using System;
+
+namespace LeandroAT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dias!");
+            Console.WriteLine(CalculoDeData.DifEntreDatasEmDias(1,1,2020,1,2,2020));        }
+    }
+}
